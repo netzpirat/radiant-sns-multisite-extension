9472b1594ce098a4a41c19fe346cb2b1d1bf3a25
will <will@spanner.org>
2009-10-27 10:57:12 +0000

    small updates to match changes to multi_site

--- sns_multisite_extension.rb
+++ sns_multisite_extension.rb
@@ -3,9 +3,18 @@ class SnsMultisiteExtension < Radiant::Extension
   description "Scopes javascripts and stylesheets to sites. It requires the spanner fork of multi_site"
   url "http//github.com/netzpirat/radiant-sns-multisite-extension"
   
+  extension_config do |config|
+    config.extension 'sns'
+    config.extension 'multi_site'
+    config.extension 'submenu'
+  end
+  
   def activate
     TextAsset.send :is_site_scoped
-    admin.text_assets.index.add :top, "admin/shared/site_jumper"
+    admin.tabs.remove "CSS"
+    admin.tabs.remove "JS"
+    admin.tabs['Pages'].add_link('stylesheets', '/admin/css', :visibility => [:admin, :developer])
+    admin.tabs['Pages'].add_link('javascripts', '/admin/js', :visibility => [:admin, :developer])
   end
   
 end
